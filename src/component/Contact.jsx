12fa369e6b7f1c8717c565cd618e0d5d86b5efb1
unknown <rahularora993676@gmail.com>
2023-03-19 22:58:36 -0700

    project

---
+++ src/component/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+const Contact = () => {
+
+  return (
+    <>
+    <Navbar/>
+    <div style={{margin:"50px auto"}}>
+        <h1 style={{textAlign:"center",color:"red"}}>Contact Component is Under Development</h1>
+    </div>
+    </>
+  )
+}
+
+export default Contact
